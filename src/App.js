74bc5972e5c6f3c8aa2dc2daf3057036c1ca5f82
Jairus San Juan <jrscerda@gmail.com>
2022-06-17 17:26:12 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import './default.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Xurpas</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container className="custom-container">
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
