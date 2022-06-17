eb5f65c5d244736e41915731c6452a8482172fc5
Jairus San Juan <jrscerda@gmail.com>
2022-06-17 17:51:19 +0800

    added modal to its own file

--- src/App.js
+++ src/App.js
@@ -7,40 +7,8 @@ import {
   Button,
   Modal,
 } from "react-bootstrap";
-import { useState } from "react";
+import Receipt from "./modal";
 import "./default.css";
-
-function Receipt() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
 
 function App() {
   return (
